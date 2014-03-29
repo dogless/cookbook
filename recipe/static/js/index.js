712395965f374be3fe591b57893c76509cae919c
me <93leonlee@gmail.com>
2014-03-29 13:02:52 -0400

    add the four movement methods

--- recipe/static/js/index.js
+++ recipe/static/js/index.js
@@ -1,6 +1,35 @@
-function changeActive(){
+function moveActiveLeft(){
 	var index = $( ".active" ).index();
-	$("li").get(index).className = "";
-	$("li").get(index+1).className = "active";
+	if (index != 0){
+		$("li").get(index).className = "";
+		$("li").get(index-1).className = "active";
+	}
 }
 
+function moveActiveRight(){
+	var lis = $("#recipes li");
+	var index = $( ".active" ).index();
+
+	if (index != lis.length-1){
+		$("li").get(index).className = "";
+		$("li").get(index+1).className = "active";
+	}
+}
+
+function moveActiveDown(){
+	var lis = $("#recipes li");
+	var index = $( ".active" ).index();
+
+	if (index < lis.length-5){
+		$("li").get(index).className = "";
+		$("li").get(index+5).className = "active";
+	{
+}
+
+function moveActiveUp(){
+	var index = $( ".active" ).index();
+	if (index - 5 > 0){
+		$("li").get(index).className = "";
+		$("li").get(index-5).className = "active";
+	}
+}
