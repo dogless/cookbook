9dde2077505a5cedb412cdb30b77803c937bd5a7
Leon Lee <93leonlee@gmail.com>
2014-03-29 01:30:22 -0400

    add recipe and step model

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+	name = models.CharField(max_length=255)
+
+class Step(models.Model):
+	number = models.IntegerField()
+	recipe = models.ForeignKey(Recipe)
+
+	class Meta:
+		ordering = ('number',)
