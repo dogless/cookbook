0db7c55883c2ee05cba6284ff5038febae58f4ce
Leon Lee <93leonlee@gmail.com>
2014-03-28 22:08:03 -0400

    remove extra base dir

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -99,10 +99,10 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-		    os.path.join(BASE_DIR, 'static'),
-		    )
+    os.path.join(BASE_DIR, 'static'),
+)
