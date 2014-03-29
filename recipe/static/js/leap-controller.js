0cc3abc277fe290961c1de153dc4dda6951151da
Doug <dougjchau@gmail.com>
2014-03-29 13:50:56 -0400

    delete garbage

--- recipe/static/js/leap-controller.js
+++ recipe/static/js/leap-controller.js
@@ -190,30 +190,3 @@ Leap.loop(controllerOptions, function(frame) {
   // Store frame for motion functions
   // previousFrame = frame;
 })
-
-function vectorToString(vector, digits) {
-  if (typeof digits === "undefined") {
-    digits = 1;
-  }
-  return "(" + vector[0].toFixed(digits) + ", "
-             + vector[1].toFixed(digits) + ", "
-             + vector[2].toFixed(digits) + ")";
-}
-
-function togglePause() {
-  paused = !paused;
-
-  if (paused) {
-    document.getElementById("pause").innerText = "Resume";
-  } else {
-    document.getElementById("pause").innerText = "Pause";
-  }
-}
-
-function pauseForGestures() {
-  if (document.getElementById("pauseOnGesture").checked) {
-    pauseOnGesture = true;
-  } else {
-    pauseOnGesture = false;
-  }
-}
