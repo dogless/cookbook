9687a4240407a685eb953141b9b2f4811f734003
me <93leonlee@gmail.com>
2014-03-29 21:48:35 -0400

    add ingredients to admin

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from recipe.models import Recipe, Step
+from recipe.models import Recipe, Step, Ingredient
 
 # Register your models here.
 admin.site.register(Recipe)
 admin.site.register(Step)
+admin.site.register(Ingredient)
